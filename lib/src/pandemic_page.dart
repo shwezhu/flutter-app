be78593c7ce2688ce910160660d75033180e7e75
shaowen <shwezhu@qq.com>
2022-01-25 15:25:18 +0800

    chore: update .gitignore and rename pandemic.dart

---
+++ lib/src/pandemic_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class PandemicPage extends StatefulWidget {
+  const PandemicPage(Key? key) : super(key: key);
+
+  @override
+  State createState() {
+    return _PandemicPageState();
+  }
+}
+
+class _PandemicPageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return const Center( child: Text('1'));
+  }
+
+}
