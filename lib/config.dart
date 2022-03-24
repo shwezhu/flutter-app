5bb62506966c2e1fba2ab9113c79040e1104d115
shaowen <shwezhu@qq.com>
2022-03-24 16:01:16 +0800

    chore: rename some variable

--- lib/config.dart
+++
@@ -1,5 +0,0 @@
-class Config {
-  static String appId = "11cf5b769a724ae447faf9516eb0973f";
-  static String apiKey = "595290127cc7816b6965063178534faf";
-  static String visitorsTable = "https://api.bmob.cn/1/classes/visitors_information";
-}
